cbda86a8147a83da3c1c318522630f0ded013c71
isabeldonner <55292440+isabeldonner@users.noreply.github.com>
2020-01-15 13:14:44 -0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,13 @@
+var j;
+for(j=0; j<8; j++){
+  var ans=Math.sqrt(2,j);
+  document.write(ans+"<br>");
+}
+var ind=0;
+while(ind<8{
+  var ans=Math.random();
+  document.write(ans+"<br>")
+  ind++
+}  
 
+  
